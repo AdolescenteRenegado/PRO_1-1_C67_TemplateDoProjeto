758de80332b0852bfb4dc17c9d74f90f105ff3f9
AdolescenteRenegado <99355190+AdolescenteRenegado@users.noreply.github.com>
2023-05-27 10:19:48 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,14 @@
 import firebase from 'firebase';
 
 // adicione SDK do Firebase
-var firebaseConfig = {
-
+const firebaseConfig = {
+  apiKey: "AIzaSyA15WYZ7OcB6eIDViXpq5g-mmBdh8qGtNU",
+  authDomain: "votacao-lula-vs-bolsonaro.firebaseapp.com",
+  databaseURL: "https://votacao-lula-vs-bolsonaro-default-rtdb.firebaseio.com",
+  projectId: "votacao-lula-vs-bolsonaro",
+  storageBucket: "votacao-lula-vs-bolsonaro.appspot.com",
+  messagingSenderId: "106956140099",
+  appId: "1:106956140099:web:e09f716457586b1720eef6"
 };
 // Initialice o Firebase
 firebase.initializeApp(firebaseConfig);
